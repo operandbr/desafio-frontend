0ca57cfed2f11b3cd1e926105468837a581faaf9
Thyago Rafael <thyago@agenciasys.com.br>
2016-06-30 09:25:16 -0300

    Add starting public files.

---
+++ public/server.php
@@ -0,0 +1,28 @@
+<?php
+require('../ScheduleDatabase.php');
+$db = new ScheduleDatabase();
+
+function insert()
+{
+    // $db->insert([]);
+}
+
+function delete()
+{
+    // $db->delete(1);
+}
+
+function update()
+{
+    // $db->update([]);
+}
+
+function get()
+{
+    // $db->fetch([]);
+}
+
+function getOne()
+{
+    // $db->fetchOne(1);
+}
